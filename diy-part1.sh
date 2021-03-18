03c09c91e33d580cd17b5597b318d801ef6fb2cd
Jinyong_Pan <1301914984@qq.com>
2021-03-18 23:42:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git
-git clone https://github.com/destan19/OpenAppFilter.git
+git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git package/bandwidthd
+git clone https://github.com/destan19/OpenAppFilter.git package/oaf
